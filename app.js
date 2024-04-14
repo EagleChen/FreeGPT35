2a4619d4351780ed0d0eb0e0cdc4c5d78bf18897
Eagle Chen <chygr1234@gmail.com>
2024-04-15 00:51:57 +0800

    fix: fix various bugs

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import { AutoRouter, cors } from 'itty-router'
-import axios from "axios";
+// import axios from "axios";
 // import https from "https";
 // import { randomUUID } from "crypto";
 
@@ -32,9 +32,10 @@ function GenerateCompletionId(prefix = "cmpl-") {
 }
 
 async function* chunksToLines(chunksAsync) {
+  const decoder = new TextDecoder('utf-8');
   let previous = "";
   for await (const chunk of chunksAsync) {
-    const bufferChunk = Buffer.isBuffer(chunk) ? chunk : Buffer.from(chunk);
+    const chunkString = decoder.decode(chunk, { stream: true });
     previous += bufferChunk;
     let eolIndex;
     while ((eolIndex = previous.indexOf("\n")) >= 0) {
@@ -60,46 +61,68 @@ async function* StreamCompletion(data) {
 }
 
 // Setup axios instance for API requests with predefined configurations
-const axiosInstance = axios.create({
-  // httpsAgent: new https.Agent({ rejectUnauthorized: false }),
-  headers: {
-    accept: "*/*",
-    "accept-language": "en-US,en;q=0.9",
-    "cache-control": "no-cache",
-    "content-type": "application/json",
-    "oai-language": "en-US",
-    origin: baseUrl,
-    pragma: "no-cache",
-    referer: baseUrl,
-    "sec-ch-ua":
-      '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": '"Windows"',
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "same-origin",
-    "user-agent":
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
-  },
-});
+// const axiosInstance = axios.create({
+//   httpsAgent: new https.Agent({ rejectUnauthorized: false }),
+//   headers: {
+//     accept: "*/*",
+//     "accept-language": "en-US,en;q=0.9",
+//     "cache-control": "no-cache",
+//     "content-type": "application/json",
+//     "oai-language": "en-US",
+//     origin: baseUrl,
+//     pragma: "no-cache",
+//     referer: baseUrl,
+//     "sec-ch-ua":
+//       '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
+//     "sec-ch-ua-mobile": "?0",
+//     "sec-ch-ua-platform": '"Windows"',
+//     "sec-fetch-dest": "empty",
+//     "sec-fetch-mode": "cors",
+//     "sec-fetch-site": "same-origin",
+//     "user-agent":
+//       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+//   },
+// });
+
+const defaultHeaders = {
+  Accept: "*/*",
+  "accept-language": "en-US,en;q=0.9",
+  "Content-Type": "application/json",
+  "oai-language": "en-US",
+  origin: baseUrl,
+  referer: baseUrl + "/",
+  "User-Agent":
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+};
+
 
 // Function to get a new session ID and token from the OpenAI API
 async function getNewSessionId() {
   let newDeviceId = crypto.randomUUID();
-  const response = await axiosInstance.post(
-    `${baseUrl}/backend-anon/sentinel/chat-requirements`,
-    {},
-    {
-      headers: { "oai-device-id": newDeviceId },
-    }
-  );
+  const response = await fetch(`${baseUrl}/backend-anon/sentinel/chat-requirements`, {
+    method: 'POST',
+    headers: {
+      'oai-device-id': newDeviceId,
+      ...defaultHeaders,
+    },
+    body: JSON.stringify({"conversation_mode_kind":"primary_assistant"}),
+  });
+  // const response = await axiosInstance.post(
+  //   `${baseUrl}/backend-anon/sentinel/chat-requirements`,
+  //   {},
+  //   {
+  //     headers: { "oai-device-id": newDeviceId },
+  //   }
+  // );
   console.log(
     `System: Successfully refreshed session ID and token. ${
       !token ? "(Now it's ready to process requests)" : ""
     }`
   );
+  console.log(`newDeviceId: ${newDeviceId}, response: ${JSON.stringify(response)}`);
   oaiDeviceId = newDeviceId;
   token = response.data.token;
+  console.log(`deviceid: ${oaiDeviceId}, token: ${token}`);
 }
 
 function checkAuthorizationHeader(req) {
@@ -163,37 +186,48 @@ async function handleChatCompletion(request) {
         author: { role: message.role },
         content: { content_type: "text", parts: [message.content] },
       })),
-      parent_message_id: randomUUID(),
+      parent_message_id: crypto.randomUUID(),
       model: "text-davinci-002-render-sha",
       timezone_offset_min: -180,
       suggestions: [],
       history_and_training_disabled: true,
       conversation_mode: { kind: "primary_assistant" },
-      websocket_request_id: randomUUID(),
+      websocket_request_id: crypto.randomUUID(),
     };
 
-    const response = await axiosInstance.post(apiUrl, body, {
-      responseType: "stream",
+    const resp = await fetch(apiUrl, {
+      method: 'POST',
       headers: {
-        "oai-device-id": oaiDeviceId,
-        "openai-sentinel-chat-requirements-token": token,
+        'oai-device-id': oaiDeviceId,
+        'Openai-Sentinel-Chat-Requirements-Token': token,
+        ...defaultHeaders
       },
+      body: JSON.stringify(body),
     });
+    // const response = await axiosInstance.post(apiUrl, body, {
+    //   responseType: "stream",
+    //   headers: {
+    //     "oai-device-id": oaiDeviceId,
+    //     "openai-sentinel-chat-requirements-token": token,
+    //   },
+    // });
 
     // Set the response headers based on the request type
+    const respHeaders = {};
     if (req.body.stream) {
-      res.setHeader("Content-Type", "text/event-stream");
-      res.setHeader("Cache-Control", "no-cache");
-      res.setHeader("Connection", "keep-alive");
+      respHeaders["Content-Type"] = "text/event-stream";
+      respHeaders["Cache-Control"] = "no-cache";
+      respHeaders["Connection"] = "keep-alive";
     } else {
-      res.setHeader("Content-Type", "application/json");
+      respHeaders["Content-Type"] = "application/json";
     }
 
     let fullContent = "";
     let requestId = GenerateCompletionId("chatcmpl-");
     let created = Date.now();
 
-    for await (const message of StreamCompletion(response.data)) {
+    const respBody = [];
+    for await (const message of StreamCompletion(resp.body)) {
       // Skip heartbeat detection
 			if (message.match(/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{6}$/)) continue;
 
@@ -227,14 +261,14 @@ async function handleChatCompletion(request) {
           ],
         };
 
-        res.write(`data: ${JSON.stringify(response)}\n\n`);
+        respBody.push(`data: ${JSON.stringify(response)}\n\n`);
       }
 
       fullContent = content.length > fullContent.length ? content : fullContent;
     }
 
     if (req.body.stream) {
-      res.write(
+      respBody.push(
         `data: ${JSON.stringify({
           id: requestId,
           created: created,
@@ -252,7 +286,7 @@ async function handleChatCompletion(request) {
         })}\n\n`
       );
     } else {
-      res.write(
+      respBody.push(
         JSON.stringify({
           id: requestId,
           created: created,
@@ -277,12 +311,14 @@ async function handleChatCompletion(request) {
       );
     }
 
-    res.end();
+    const response = new Response(respBody, {status: 200});
+    for (const key of Object.keys(respHeaders)) {
+      response.headers.set(key, respHeaders[key]);
+    }
+
+    return response;
   } catch (error) {
-    // console.log('Error:', error.response?.data ?? error.message);
-    if (!res.headersSent) res.setHeader("Content-Type", "application/json");
-    // console.error('Error handling chat completion:', error);
-    res.write(
+    const response = new Response(
       JSON.stringify({
         status: false,
         error: {
@@ -290,9 +326,11 @@ async function handleChatCompletion(request) {
             "An error happened, please make sure your request is SFW, or use a jailbreak to bypass the filter.",
           type: "invalid_request_error",
         },
-      })
+      }), {status: 500}
     );
-    res.end();
+    response.headers.set("Content-Type", "application/json");
+
+    return response;
   }
 }
 
@@ -307,12 +345,12 @@ const router = AutoRouter({
 router.post("/v1/chat/completions", handleChatCompletion);
 
 // 404 handler for unmatched routes
-router.all("*", () => new Response({
+router.all("*", () => new Response(JSON.stringify({
   status: false,
   error: {
     message: `The requested endpoint was not found. please make sure to use "http://localhost:3040/v1" as the base URL.`,
     type: "invalid_request_error",
   },
-}, { status: 404 }))
+}), { status: 404 }))
 
 export default { ...router };
